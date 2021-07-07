9cb8ee67aa6dacb4369505ee961c3a3934eb69bb
osxzhg <osxzhg@gmail.com>
2021-07-07 22:35:58 +1200

    initial commit

---
+++ WpfSuperDemo/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSuperDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Joe", LastName = "Smith" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm" });
+            myComboBox.ItemsSource = people;
+            ComboBox cbx = new ComboBox();
+            cbx.Name = "ComboBox1";
+            cbx.Width = 400;
+            cbx.Height = 30;
+            cbx.HorizontalAlignment = HorizontalAlignment.Center;
+            ComboBoxItem item = new ComboBoxItem();
+            item.Content = "Coffie";
+            cbx.Items.Add(item);
+            cbx.Items.Add("Tea");
+            cbx.Items.Add("Orange");
+            cbx.Items.Add("Milk");
+            cbx.Items.Add("Iced Tea");
+            cbx.Items.Add("Mango Shake");
+            RootLayout.Children.Add(cbx);
+        }
+
+        private void sumbitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {firstNameText.Text}");
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
